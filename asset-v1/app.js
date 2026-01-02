217d199990a076f096ad645deddc2ae030ceff52
mrcookeee <official.inkredaman@gmail.com>
2026-01-02 14:18:15 -0600

    Update app.js

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -1,6 +1,6 @@
 /* amogus sussy baka deez nuts kofitezed */
 var app = new function() {
-    this.name = "The Last Day", this.version = "Unreleased v1", this.date = "2023", this.folder = "asset-v1/", this.looptime = 9600, this.bpm = 100, this.totalframe = 460, this.nbpolo = 7, this.nbloopbonus = 2, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 34, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#0f1521", this.col0 = "#828da0", this.col1 = "#596988", this.col2 = "#384863", this.col3 = "#202a3c", this.col4 = "#131a2a", this.animearray = [{
+    this.name = "Solar", this.version = "The Solarium v1", this.date = "2023", this.folder = "asset-v1/", this.looptime = 9600, this.bpm = 100, this.totalframe = 460, this.nbpolo = 7, this.nbloopbonus = 2, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 34, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#0f1521", this.col0 = "#828da0", this.col1 = "#596988", this.col2 = "#384863", this.col3 = "#202a3c", this.col4 = "#131a2a", this.animearray = [{
         name: "01kick",
         color: "af8553",
         uniqsnd: !1
