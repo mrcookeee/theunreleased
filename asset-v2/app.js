cee2684a4da54aced7b3f5d3226046956809f6d1
mrcookeee <official.inkredaman@gmail.com>
2026-01-05 17:22:42 -0600

    Update app.js

--- asset-v2/app.js
+++ asset-v2/app.js
@@ -1,6 +1,6 @@
 /* amogus sussy baka deez nuts kofitezed */
 var app = new function() {
-    this.name = "Pop Zey", this.version = "Unreleased v2", this.date = "2024", this.folder = "asset-v2/", this.looptime = 8000, this.bpm = 120, this.totalframe = 384, this.nbpolo = 7, this.nbloopbonus = 2, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 34, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#0b252b", this.col0 = "#a5d7c6", this.col1 = "#6dafa3", this.col2 = "#2f6c6a", this.col3 = "#235052", this.col4 = "#18373d", this.animearray = [{
+    this.name = "uh", this.version = "Unreleased v3", this.date = "2024", this.folder = "asset-v2/", this.looptime = 8000, this.bpm = 120, this.totalframe = 384, this.nbpolo = 7, this.nbloopbonus = 2, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 34, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#0b252b", this.col0 = "#a5d7c6", this.col1 = "#6dafa3", this.col2 = "#2f6c6a", this.col3 = "#235052", this.col4 = "#18373d", this.animearray = [{
         name: "01tshkuh",
         color: "17363c",
         uniqsnd: !0
